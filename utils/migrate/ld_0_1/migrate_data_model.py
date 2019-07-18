6ff5b1a38d4527dcaa9603e56a801ee5beb64c4a
Rakshit Ramesh <rakshitadmar@gmail.com>
2019-07-18 13:22:30 +0530

    Added 0.1 migration script

--- utils/migrate/ld_0_1/migrate_data_model.py
+++ utils/migrate/ld_0_1/migrate_data_model.py
@@ -12,7 +12,10 @@ dm_name = dm_list[-1]
 path_to_dm = dm_list[dm_list.index("data_models"):-1]
 path_to_dm_folder = "".join(a+"/" for a in dm_list[:-1])
 folder_path = "".join(a+"/" for a in path_to_dm)
+dm_context_file_name =  dm_list[-2] + "_context.json"
+path_to_dm_context = "".join(a + "/" for a in dm_list[:-1]) + dm_context_file_name
 print(folder_path)
+print(path_to_dm_context)
 
 
 core_context = "https://raw.githubusercontent.com/iudx/iudx-ld/master/base_schemas/v0.0.0/core_context.json"
@@ -44,10 +47,13 @@ dm_context["@context"] = {}
 for prop in props:
     if(re.search('time', prop, re.IGNORECASE)):
         dm_context["@context"][prop] = {"@id":dm_context_url + prop, "@type": "TimeProperty"}
+    elif(re.search('location', prop, re.IGNORECASE)):
+        dm_context["@context"][prop] = {"@id":dm_context_url + prop, "@type": "GeoProperty"}
     else:
         dm_context["@context"][prop] = {"@id":dm_context_url + prop, "@type": "Property"}
     if("describes" in props[prop].keys()):
-        dm_context[prop] = props[prop]["describes"]
+        dm_context[prop] = {}
+        dm_context[prop]["describes"] = props[prop]["describes"]
     if("units" in props[prop].keys()):
         props[prop]["unitText"] = props[prop]["units"]
     if("oneOf" in props[prop].keys()):
@@ -56,4 +62,12 @@ for prop in props:
         props[prop].pop("valueSchema")
 
 
-print(json.dumps(dm, indent=4, sort_keys=True))
+#print(json.dumps(dm, indent=4, sort_keys=True))
+#print(json.dumps(dm_context, indent=4, sort_keys=True))
+
+p = path_to_dm_folder + dm_name
+with open(p, "w") as f:
+    json.dump(dm, f, indent=4, sort_keys=True)
+
+with open(path_to_dm_context, "w") as f:
+    json.dump(dm_context, f, indent=4, sort_keys=True)
