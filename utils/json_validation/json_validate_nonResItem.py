f6964e5b4023a8da7cb83074464c7b2824fe34bd
Abhay Sharma <abhay.bits@gmail.com>
2019-07-27 19:17:39 +0530

    Added a validation for an cat item with no data model and only reference schema

---
+++ utils/json_validation/json_validate_nonResItem.py
@@ -0,0 +1,51 @@
+import jsonschema
+import json
+import sys
+import requests
+
+#schemaFile = "base_schemas/iudx_resourceItem_schema.json"
+#itemFile = "ex_items/testItem.json"
+
+#schemaFile = "../base_schemas/iudx_resourceItem_schema.json"
+
+itemFile = sys.argv[1]
+
+#schemaFile = "../data_models/aqm.json"
+#itemFile = "../data_models/aqm_item.json"
+
+
+with open(itemFile, "r") as f:
+    item = json.load(f)
+
+
+base_schema = {}
+bs_fl = item["refBaseSchema"]["object"]
+
+
+
+schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "allOf": [
+   {"$ref": bs_fl}
+  ]
+}
+
+# This will find the correct validator and instantiate it using the resolver.
+# Requires that your schema a line like this: "$schema": "http://json-schema.org/draft-04/schema#"
+try:
+ jsonschema.validate(item, schema )
+
+except jsonschema.exceptions.ValidationError as errV:
+ print ("Validation Error Occured")
+ #print errV
+ v = jsonschema.Draft7Validator(schema, types=(), format_checker=None)
+ #for error in sorted(v.iter_errors(data), key=str):
+ #    print(error.message)
+ errors = sorted(v.iter_errors(item), key=lambda e: e.path)
+ for error in errors:
+     print (error.message)
+
+except jsonschema.exceptions.SchemaError as errS:
+ print ("Schema Error Occured")
+ print (errS.message)
